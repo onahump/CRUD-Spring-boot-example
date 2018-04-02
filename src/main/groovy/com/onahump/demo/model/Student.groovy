b2c9149bca3ca88724f7494e17660c2960c6338d
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-04-01 22:00:39 -0500

    Solving conflicts in Student model

--- src/main/groovy/com/onahump/demo/model/Student.groovy
+++ src/main/groovy/com/onahump/demo/model/Student.groovy
@@ -1,17 +1,25 @@
-package com.onahump.demo
+package com.onahump.demo.model
 
-import java.persistence.*
+import javax.persistence.Id
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.validation.constraints.NotNull
 
+@Entity
 class Student{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	Integer id 
-
+	
+	@NotNull
 	String name
 
+	@NotNull
 	String lastName
 
+	@NotNull
 	Date date
 
 }
